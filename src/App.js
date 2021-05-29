2779b1a7d260a62b0ea7dcf3f6cceb2d6d582bf9
AdityaSinghRathore <goforaditya@gmail.com>
2021-05-29 16:02:05 +0530

    Added SSL

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     const [stockData, setStockData] = useState({})
 
     useEffect(() => {
-        fetch("http://40.80.83.219/api/gainers")
+        fetch("https://40.80.83.219/api/gainers")
             .then((res) => res.json())
             .then((data) => {
                 setGainers(data)
@@ -28,7 +28,7 @@ function App() {
 
     const getPredctions = () => {
         let stock = symbol
-        fetch("http://40.80.83.219/api/predict/" + stock)
+        fetch("https://40.80.83.219/api/predict/" + stock)
             .then((res) => res.json())
             .then((data) => {
                 setStockData({
